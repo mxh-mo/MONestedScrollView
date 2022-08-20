543f950af7d2e57cb2c0036b5c6fe81405e8af48
mikimo <mikimo@tencent.com>
2022-08-20 21:39:13 +0800

    add: nested scrollView demo

---
+++ MONestedScrollView/MOSubScrollExecutor.swift
@@ -0,0 +1,142 @@
+//
+//  MOSubScrollExecutor.swift
+//  MODemoSwift
+//
+//  Created by mikimo on 2022/8/20.
+//
+
+import UIKit
+
+class MOSubScrollExecutor: NSObject {
+    
+    // MARK: - Public
+
+    public var mainScrollSuperView: UIView?
+
+    // MARK: - 主 ScrollView 的滑动回调
+    
+    public func mainScrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.mainScrollView = scrollView
+        /// 记录拖拽前的偏移
+        self.mainScrollOffsetBeforeDragging = scrollView.contentOffset
+    }
+
+    public func mainScrollViewDidScroll(_ scrollView: UIScrollView) {
+        if self.mainScrollEnable {
+            /// 需要重新布局，重新计算 headerView 和 containerView 的高度
+            /// 触发 MONestedScrollViewController 的 viewDidLayoutSubviews 方法
+            self.mainScrollSuperView?.setNeedsLayout()
+            return
+        }
+        /// 不可滑动时，重置偏移
+        self.updateScrollView(scrollView, self.mainScrollOffsetBeforeDragging)
+    }
+    
+    // MARK: - 内部 ScrollView 的滑动回调
+
+    public func subScrollWillBeginDragging(_ scrollView: UIScrollView) {
+        /// 切换tab时重置标记位
+        if self.currentSubScrollView != nil &&
+            !self.currentSubScrollView!.isEqual(scrollView) {
+            print("subScrollDidChange")
+            self.mainScrollEnable = true
+        }
+        self.currentSubScrollView = scrollView
+        self.subScrollViewPreOffset = scrollView.contentOffset
+    }
+
+    public func subScrollDidScroll(_ scrollView: UIScrollView) {
+        /// 丢弃其他scrollView的回调(case: 刚拖拽完tabView，立马切换到webView，此时还会收到tabView的滑动回调)
+        if !scrollView.isEqual(self.currentSubScrollView) {
+            return
+        }
+        if scrollView.contentOffset.y.isEqual(to: self.subScrollViewPreOffset.y) {
+            return
+        }
+        let pullDown: Bool = scrollView.contentOffset.y < self.subScrollViewPreOffset.y
+        if pullDown {
+            self.handlePullDown(scrollView) /// 处理下拉
+        } else {
+            self.handlePullUp(scrollView)   /// 处理上拉
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    /// 下拉: list 先拉到顶，再放大 headerView
+    func handlePullDown(_ scrollView: UIScrollView) {
+        print("下拉 subScrollDidScroll: \(scrollView.contentOffset.y)")
+        
+        /// 还没拉到顶 或 headerView已是最大状态，允许subScrollView滑动，不做处理
+        if scrollView.contentOffset.y > 0 ||
+            self.headerIsMaxState() {
+            self.mainScrollEnable = false
+            self.subScrollViewPreOffset = scrollView.contentOffset
+        } else {
+            /// 拉到顶部了 且 播放器需要放大
+            print("拉到顶部了 & 播放器需要放大")
+            self.mainScrollEnable = true
+            
+            /// 重置偏移(放大player时，不需要下拉刷新效果)
+            self.updateScrollView(scrollView, .zero)
+            self.subScrollViewPreOffset = .zero
+        }
+    }
+    
+    /// pullUp 上拉: 先缩小播放器，再拉 list
+    func handlePullUp(_ scrollView: UIScrollView) {
+        print("上拉 subScrollDidScroll: \(scrollView.contentOffset.y)")
+        
+        /// headerView 已是最小状态，允许subScrollView滑动，不做处理
+        if self.headerIsMinState() {
+            self.mainScrollEnable = false
+            self.subScrollViewPreOffset = scrollView.contentOffset
+            return
+        }
+        self.mainScrollEnable = true
+        if scrollView.contentOffset.y <= 0 { /// 忽略下拉刷新的回弹(否则死循环)
+            return
+        }
+        print("heanderView缩小时，重置subScrollView偏移")
+        self.updateScrollView(scrollView, self.subScrollViewPreOffset)
+    }
+
+    func headerIsMinState() -> Bool {
+        guard let mainScrollView = self.mainScrollView else {
+            return true
+        }
+        return mainScrollView.contentOffset.y.isEqual(to: 0.0)
+    }
+
+    func headerIsMaxState() -> Bool {
+        guard let mainScrollView = self.mainScrollView else {
+            return true
+        }
+        return mainScrollView.contentInset.top.isEqual(to: abs(mainScrollView.contentOffset.y))
+    }
+    
+    // MARK: - Helper Methods
+    
+    /// 更新 scrollView 的 offset, 相同时跳过，防止极限情况死循环
+    private func updateScrollView(_ scrollView: UIScrollView, _ offset: CGPoint) {
+        if scrollView.contentOffset.equalTo(offset) {
+            return
+        }
+        scrollView.contentOffset = offset;
+    }
+    
+    // MARK: - Private Properties
+    
+    /// 用于判断其最大最小状态
+    private var mainScrollView: UIScrollView?
+    /// 记录拖拽前的偏移，用于不可滑动状态时，重置偏移
+    private var mainScrollOffsetBeforeDragging: CGPoint = .zero
+    /// 是否处于可滑动状态
+    private var mainScrollEnable: Bool = true
+
+    /// 用于防重入
+    private var currentSubScrollView: UIScrollView?
+    /// 记录拖拽前的偏移，用于不可滑动状态时，重置偏移
+    private var subScrollViewPreOffset: CGPoint = .zero
+
+}
---
+++ MONestedScrollView/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  MONestedScrollView
+//
+//  Created by mikimo on 2022/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    // MARK: - Initail Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.mainScrollView)
+        self.mainScrollView.addSubview(self.tabsContainerCtl.view)
+        self.view.addSubview(self.headerView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.mainScrollView.contentOffset = CGPoint(x: 0.0, y: 0.0)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let viewSize = self.view.bounds.size
+        let safeInset = self.view.safeAreaInsets
+        let containerWidth = viewSize.width - safeInset.left - safeInset.right
+        let containerHeight = viewSize.height - safeInset.top - safeInset.bottom
+
+        let mainScrollView = self.mainScrollView
+        let headerView = self.headerView
+        let tabsContainerView = self.tabsContainerCtl.view
+        
+        /// 铺满
+        mainScrollView.frame = CGRect(x: safeInset.left,
+                                      y: safeInset.top,
+                                      width: containerWidth,
+                                      height: containerHeight)
+        mainScrollView.contentSize = CGSize(width: containerWidth,
+                                            height: containerHeight)
+        /// 内边距为可滑动值
+        let scrollTopInset = headerViewMaxHeight - headerViewMinHeight
+        mainScrollView.contentInset = UIEdgeInsets(top: scrollTopInset,
+                                                   left: 0.0,
+                                                   bottom: 0.0,
+                                                   right: 0.0)
+        /// 高度根据偏移
+        let headerHeight = headerViewMinHeight + abs(mainScrollView.contentOffset.y)
+        headerView.frame = CGRect(x: safeInset.left,
+                                  y: safeInset.top,
+                                  width: containerWidth,
+                                  height: headerHeight)
+        /// 高度等于剩下的范围
+        tabsContainerView?.frame = CGRect(x: 0.0,
+                                          y: headerViewMinHeight,
+                                          width: containerWidth,
+                                          height: containerHeight - headerHeight)
+    }
+    
+    // MARK: - Private Methods - 主 ScrollView 的回调事件
+
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.scrollExecutor.mainScrollViewWillBeginDragging(scrollView)
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.scrollExecutor.mainScrollViewDidScroll(scrollView)
+    }
+    
+    // MARK: - Private Properties
+    
+    private var headerViewMinHeight: CGFloat = 100.0
+    private var headerViewMaxHeight: CGFloat = 200.0
+
+    // MARK: - Getter Methods
+    
+    private lazy var mainScrollView: MOMultiResponseScrollView = {
+        let scroll = MOMultiResponseScrollView(frame: .zero)
+        scroll.delegate = self
+        scroll.bounces = false
+        scroll.backgroundColor = .blue
+        return scroll
+    }()
+
+    private lazy var headerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .red
+        return view
+    }()
+
+    private lazy var tabsContainerCtl: MOMultiTabContainerViewController = {
+        let ctl = MOMultiTabContainerViewController(nibName: nil, bundle: nil)
+        /// 内部 ScrollView 的回调事件
+        ctl.willBeginDragging = { [weak self] (scrollView: UIScrollView) in
+            self?.scrollExecutor.subScrollWillBeginDragging(scrollView)
+        }
+        ctl.didScroll = { [weak self] (scrollView: UIScrollView) in
+            self?.scrollExecutor.subScrollDidScroll(scrollView)
+        }
+        ctl.view.backgroundColor = .cyan
+        return ctl
+    }()
+    
+    private lazy var scrollExecutor: MOSubScrollExecutor = {
+        let exector = MOSubScrollExecutor()
+        exector.mainScrollSuperView = self.view
+        return exector
+    }()
+}
+
